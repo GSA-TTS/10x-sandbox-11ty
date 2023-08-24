c29287a658f2657a34152aece53aad118c0e6d9c
jimmoffet <jim.moffet@gsa.gov>
2023-08-24 13:40:54 -0700

    working deploy with more minimal, non-USWDS, repo

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	"title": "Eleventy | Kinsta Applicaton Hosting Starter",
+	"url": "https://example.com/",
+	"language": "en",
+	"description": "It's just a Kinsta Applicaton Hosting Starter",
+	"author": {
+		"name": "Your Name Here",
+		"email": "youremailaddress@example.com",
+		"url": "https://example.com/"
+	}
+}
